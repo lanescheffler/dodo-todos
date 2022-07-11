f7eabc29d7d5ca4eb30a4d007d42c12cdfafb84b
yorkmacbook058 <lanescheffler@gmail.com>
2022-07-11 16:00:14 -0500

    initial commit & clean up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome to DODO toDO's
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="website"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          |EDITOR|
+        </a>
+        <a
+            className="App-link"
+            href="website"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          |FOLLOWER|
         </a>
       </header>
     </div>
